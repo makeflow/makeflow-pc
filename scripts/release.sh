168dcad270cc970a27edbd5e0b9bb1195751a368
SUCHMOKUO <suchmokuo@gmail.com>
2020-07-29 16:53:19 +0800

    add release script

---
+++ scripts/release.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+VERSION=$1
+
+if [ -z "$VERSION" ]; then
+  echo "'version' not set! try running 'npm run release -- <your version>'"
+  exit 1
+fi
+
+npm version "$VERSION"
+
+git add package.json
+git commit -m "release $VERSION"
+
+TAG="v$VERSION"
+
+git tag "$TAG"
+git push
+git push origin "$TAG"
